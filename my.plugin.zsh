870085d523ef9c5a98b9fdfcc8487e234371bef8
zhexuany <yangzhexuan@pingcap.com>
2018-03-03 22:52:19 +0800

    source newly added files

--- my.plugin.zsh
+++ my.plugin.zsh
@@ -2,4 +2,5 @@ source ${0:A:h}/leetcode.zsh
 source ${0:A:h}/spark.zsh
 source ${0:A:h}/git.zsh
 source ${0:A:h}/etc.zsh
-source ${0:A:h}/git.zsh
+source ${0:A:h}/work.zsh
+source ${0:A:h}/alias.zsh
