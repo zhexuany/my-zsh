b4d061d851b478d437d63a545edfa9727e0570ae
zhexuany <yangzhexuan@pingcap.com>
2018-03-14 19:18:54 +0800

    fix a spelling mistake and add parameters option

--- bench.zsh
+++ bench.zsh
@@ -1,14 +1,24 @@
-capature_flamegraph() {
+capture_flamegraph() {
+    ip="127.0.0.1"
+    port="10080"
+    if [ ! -z "$1" ]; then
+        ip=$1
+    fi
+
+    if [ ! -z "$2" ]; then
+        port=$2
+    fi
+
     if [ ! -d "FlameGraph" ]; then
-        echo "please clone FlameGraph locally"
-        echo "you can do this by executing:"
-        git clone https://github.com/brendangregg/FlameGraph.git
+       echo "please clone FlameGraph locally"
+       echo "you can do this by executing:"
+       git clone https://github.com/brendangregg/FlameGraph.git
     fi
     export PATH="~/FlameGraph:$PATH"
     if [ ! -x go-torch ]; then
-	      go get github.com/uber/go-torch
+        go get github.com/uber/go-torch
     fi
-    go-torch -u http://127.0.0.1:10080 -t 60
+    go-torch -u http://$ip:$port` -t 60
     echo $(curl --upload-file ./torch.svg https://transfer.sh/kernel.svg)
     echo \n
 }
