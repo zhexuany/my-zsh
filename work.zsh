97f9f219b9a7f82d8239d25c97caad31a797f2af
zhexuany <yangzhexuan@pingcap.com>
2019-01-19 13:01:05 +0800

    adding idc

--- work.zsh
+++ work.zsh
@@ -9,3 +9,11 @@ idc2() {
 idc3() {
   sshi pingcap@172.16.30.1
 }
+
+idc() {
+    if [ ! -z "$1" ]; then
+        ssh root@172.16.30.$1
+    else
+        echo "please supply a suffix of ip such as 90, 92"
+    fi
+}
